f8091ffe88a6fc1dd55eb9db589da471285c835e
Anshuman Kumar <136048963+Anshuman110@users.noreply.github.com>
2025-07-25 17:38:38 +0530

    Add files via upload
    sum of square of first n natural numbers.

---
+++ Logic Building Problems/squareSum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class squareSum{
+
+static int sum(int n){
+int z;
+int sum=0;
+for(int i=1 ; i<=n ; i++){
+z=i*i;
+sum+=z;
+}
+return sum;
+}
+
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("enter any no. ");
+int n=sc.nextInt();
+System.out.println(sum(n));
+}
+}

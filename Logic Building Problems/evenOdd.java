8ff743dd56d66487185c0feaf22824583697d6c1
Anshuman Kumar <136048963+Anshuman110@users.noreply.github.com>
2025-07-25 16:51:19 +0530

    Create evenOdd.java

---
+++ Logic Building Problems/evenOdd.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class evenOdd{
+
+static boolean check(int n){
+if(n%2==0){
+return true;
+}
+else{
+return false;
+}
+}
+
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("enter number: ");
+int n=sc.nextInt();
+System.out.println(check(n));
+}
+
+}

88dea5bad7c193900c243f98561fdc3898feb90b
Anshuman Kumar <136048963+Anshuman110@users.noreply.github.com>
2025-07-26 16:49:01 +0530

    Add files via upload

--- ArrayList/Main.java
+++ ArrayList/Main.java
@@ -1,1 +1,13 @@
+import java.util.ArrayList;
 
+public class Main{
+public static void main(String[] args){
+ArrayList<String> car=new ArrayList<String>();
+
+car.add("volvo");
+car.add("BMW");
+car.add("Bentley");
+
+System.out.println(car);
+}
+}

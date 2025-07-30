2f6a4567018fe9fe1bd8cd9fb21ae9bda46832e8
Anshuman Kumar <136048963+Anshuman110@users.noreply.github.com>
2025-07-30 13:15:19 +0530

    Add files via upload

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,24 @@
+public class InsertionSort{
+public static void main(String[] args){
+int arr[]={64, 34, 25, 12, 22, 11, 90, 5};
+
+int n=arr.length;
+for(int i=1; i<n; i++){
+int insertIndex = i;
+int currentValue = arr[i];
+int j=i - 1;
+
+while(j>=0 && arr[j]>currentValue){
+arr[j+1]=arr[j];
+insertIndex=j;
+j--;
+}
+arr[insertIndex]=currentValue;
+}
+
+for(Integer m:arr){
+System.out.print(m+" ");
+}
+
+}
+}
